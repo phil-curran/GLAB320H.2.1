8abbc6a28e11384e4bf3e90a546cc09483ea9382
Phil Curran <pecurran@outlook.com>
2024-03-01 10:54:15 -0800

    initial commit.

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+/* eslint-disable react/prop-types */
+import "./App.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Simple React Application</h1>
+    </div>
+  );
+};
+
+const Content = ({ color, text }) => {
+  return <p style={{ color: color }}>{text}</p>;
+};
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h1>Created by Me, of course.</h1>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div className="main">
+      <Header />
+      <div className="container">
+        <Content color="blue" text="This is my first React Application!" />
+        <Content color="red" text="Wish me luck..." />
+        <Content color="green" text="I think I've got it!" />
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
